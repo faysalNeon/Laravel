bd1ce13afd27a6d285d87453812fd5f5f67efc16
faysalNeon <faysal.neon@gmail.com>
2020-06-27 20:25:12 +0600

    init

---
+++ example/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Example PHP Code</title>
+    <style>
+        body{
+            display:flex;
+            min-height:90vh;
+            text-align:center;
+            align-items:center;
+            justify-content:center;
+        }
+        .title{
+            text-transform:uppercase;
+        }
+    </style>
+</head>
+<body>
+    <h1 class="title"> Project Example </h1>
+</body>
+</html>
