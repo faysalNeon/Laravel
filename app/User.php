a4951f0a00d26bbb82a84b166badda0c599301b7
faysalNeon <faysal.neon@gmail.com>
2020-08-09 00:14:48 +0600

    2020-08-08

--- app/User.php
+++ app/User.php
@@ -9,31 +9,34 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    use CheckActive;
     protected $fillable = [
-        'name', 'email', 'password','mobile'
+        'name', 'email', 'password'
     ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $appends=['details'];
+
+    public function getDetailsAttribute($data){
+        return $this->attributes['id'].'#'.$this->attributes['name'];
+    }
+
+    public function getEmailAttribute($data){
+        return $data;
+    }
+
+    public function setEmailAttribute($data){
+        $this->attributes['email']=$data;
+    }
+
+    public function setPasswordAttribute($data){
+        $this->attributes['password']=bcrypt($data);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use App\User;
+use Illuminate\Support\Str;
+
 Route::view('/', 'welcome');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/set',function(){
+    $name = Str::random(5);
+//    User::create([
+//        'name' =>  $name."wootap",
+//        'email' => $name."@wootap.me",
+//        'password' => "12345678"
+//    ]);
+    (new User)->jkono();
+//
+//    foreach (User::active()->get() as $res) {
+//        echo $res->name.' => '.$res->email.' => '.$res->password.' => '.$res->details.' <br/> ------------------------------ <br/>';
+//    }
+//dd(User::all());
+});
+Route::get('/clear',function(){
+   User::truncate();
+   echo "all user are clear";
+});
+
+
